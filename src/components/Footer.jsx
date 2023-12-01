b9964de89db09ea800f0f672339bf9ff78650d21
pmc-odebroker <patrickcharlestz@gmail.com>
2023-12-01 07:46:03 +0300

    design home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <ul className="footer__categories">
+        <li><Link to={"/posts/categories/Agriculture"}>Agriculture</Link></li>
+        <li><Link to={"/posts/categories/Business"}>Business</Link></li>
+        <li><Link to={"/posts/categories/Education"}>Entertainment</Link></li>
+        <li><Link to={"/posts/categories/Art"}>Art</Link></li>
+        <li><Link to={"/posts/categories/Investment"}>Investment</Link></li>
+        <li><Link to={"/posts/categories/Unigategorized"}>Unigategorized</Link></li>
+        <li><Link to={"/posts/categories/Weather"}>Weather</Link></li>
+      </ul>
+      <div className="footer__copyright">
+        <small>All Rights Reserved &copy; Copyright, patrick mao</small>
+      </div>
+        
+      </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo.jpg'
+// import { FaBars } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav__container">
+            <Link to={'/'} className="nav__logo">
+                <img className="img" src={Logo} alt="Nav logo"></img>
+            </Link>
+            <ul className="nav__menu">
+                <li><Link to={ '/profile' }>User Name</Link></li>
+                <li><Link to={ '/create' }>Create Post</Link></li>
+                <li><Link to={ '/authors' }>Author</Link></li>
+                <li><Link to={ '/logout' }>Logout</Link></li>
+                <button className="nav_toggle-btn">
+                    <AiOutlineClose />
+                </button>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
